f378f2424e0f581e3f073e52f5a28c4c5d7affc4
abauer <ambauer@utexas.edu>
2016-09-29 21:48:52 -0500

    final touches and moving to linux for testing

--- Main.java
+++ Main.java
@@ -13,79 +13,20 @@
 
 
 package assignment3;
+
 import org.junit.Test;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	// static variables and constants only here.
+
 	static HashSet<String> dictionary;
     static HashSet<String> breadthVisited;
     static HashSet<String> deapthVisited;
-    static HashMap<String,ArrayList<String>> adjacents;
-
-    @Test
-    public void initTest(){
-        initialize();
-    }
-
-    @Test
-    public void testb1(){
-        initialize();
-        printLadder(getWordLadderBFS("money","stone"));
-    }
-    @Test
-    public void testb2(){
-        initialize();
-        printLadder(getWordLadderBFS("smart","money"));
-    }
-    @Test
-    public void testb3(){
-        initialize();
-        printLadder(getWordLadderBFS("peach","wooed"));
-    }
-    @Test
-    public void testb4(){   //confirms that dfs gives a suboptimal result
-        initialize();
-        printLadder(getWordLadderBFS("wrote","amigo"));
-    }
-    @Test
-    public void testb5(){   //confirms that DFS result of no ladder is correct
-        initialize();
-        printLadder(getWordLadderBFS("whump","xylan"));
-    }
-
-
-
-    @Test
-    public void testd1(){//// TODO: 9/27/2016
-        initialize();
-        printLadder(getWordLadderDFS("SMART","MONEY"));
-    }
-    @Test
-    public void testd2(){//// TODO: 9/27/2016
-        initialize();
-        printLadder(getWordLadderDFS("MONEY","STONE"));
-    }
-    @Test
-    public void testd3(){
-        initialize();
-        printLadder(getWordLadderDFS("wrote","amigo"));
-    }
-    @Test
-    public void testd4(){ // test to determine that dfs cannot find this word ladder
-        initialize();
-        printLadder(getWordLadderDFS("whump","xylan"));
-    }
-    @Test
-    public void testd5(){// TODO: 9/27/2016
-        initialize();
-        printLadder(getWordLadderDFS("wrote","amigo"));
-    }
-
+    static HashMap<String,HashSet<String>> adjacents;
+    static String startField;
+    static String endField;
 
 	public static void main(String[] args) throws Exception {
 
@@ -100,47 +41,52 @@ public class Main {
 			kb = new Scanner(System.in);// default from Stdin
 			ps = System.out;			// default to Stdout
 		}
-		initialize();
-		printLadder(parse(kb));
+		initialize();           //init
+        ArrayList<String> temp = parse(kb); //parse input
+        ArrayList<String> result = getWordLadderBFS(temp.get(0),temp.get(1));   //obtain wordladder from BFS
+        /*if(result.isEmpty())
+		    printLadder(temp);
+        else*/
+            printLadder(result);       //print result
 	}
 	
 	public static void initialize() {
-        Set<String> temp = makeDictionary();
+        Set<String> temp = makeDictionary();        //get Dictionary from provided method
         dictionary = new HashSet<String>();
         deapthVisited = new HashSet<String>();
         breadthVisited = new HashSet<String>();
-        adjacents = new HashMap<String,ArrayList<String>>();
-        temp.stream().forEach(word -> dictionary.add(word));        //awesomeness at work
-        for(String word : temp){
-            ArrayList<String> words = new ArrayList<String>();
-            for(int i =0; i<word.length(); i++) {
-                StringBuilder sb = new StringBuilder(word);
-                for (char alpha = 'A'; alpha <= 'Z'; alpha++) {
+        adjacents = new HashMap<String,HashSet<String>>();
+        temp.stream().forEach(word -> dictionary.add(word.toUpperCase()));        //awesomeness at work //store in global dictionary
+        for(String word : dictionary) {                 //for every word
+            HashSet<String> words = new HashSet<String>();      //create a Hash set of adjacents
+            for(int i =0; i<word.length(); i++) {       //for every letter in the word
+                StringBuilder sb = new StringBuilder(word);     //create a temporary String
+                for (char alpha = 'A'; alpha <= 'Z'; alpha++) {     //change that letter to one of the other 25
                     if (alpha == word.charAt(i))
                         continue;
                     sb.setCharAt(i, alpha);
-                    if (dictionary.contains(sb.toString())) {
-                        words.add(sb.toString().toUpperCase());
+                    if (dictionary.contains(sb.toString())) {       //if temporary is in dicitonary
+                        words.add(sb.toString().toUpperCase());     //add it to adjacetns
                     }
                 }
             }
-            ArrayList<String> values = adjacents.get(word);
-            if(values==null){
-                adjacents.put(word,words);
+            HashSet<String> values = adjacents.get(word);   //get the current HashSet of word adjacent to 'word'
+            if(values==null){                           //if there are none yet
+                adjacents.put(word,words);      //directly put the list into the HashMap
             }
-            else{
-                values.addAll(words);
+            else {
+                values.addAll(words);           //otherwise add the list to the existing list
             }
         }
-	}
-	
+    }
+
 	/**
 	 * @param keyboard Scanner connected to System.in
 	 * @return ArrayList of 2 Strings containing start word and end word. 
 	 * If command is /quit, return empty ArrayList.
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
-        String input = keyboard.nextLine().trim().toUpperCase();
+        String input = keyboard.nextLine().trim().toUpperCase();    //make all upper case for simplicity
 		if(input.contains("/QUIT"))         //look for quit command
 		    System.exit(0);
         char[] chars = input.toCharArray();     //prepare to parse for two words
@@ -150,26 +96,33 @@ public class Main {
             if(chars[i]=='\n'||chars[i]==9||chars[i]==32){
                 break;
             }
-            sb.append(chars[i]);
+            sb.append(chars[i]);        //add to the first word
         }
-        String sb2 = new String(input.substring(i+1).trim());
-        ArrayList<String> value = new ArrayList<String>();
-        value.add(sb.toString()); value.add(sb2);
-        return value;
+        String sb2 = new String(input.substring(i+1).trim()); //cut the array so that we get the second word
+        ArrayList<String> value = new ArrayList<String>();      //make an arraylist for these words
+        value.add(sb.toString()); value.add(sb2);       //add both to arraylist
+        return value;       //return arraylist
 	}
 
 	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-        if (start.equals(end)) {
-            return new ArrayList<String>();
+        if (start.equals(end)) {        //if the words are equal (not tested)
+            return new ArrayList<String>();     //return empty array
         }
-        return getWLDFS(start, end);
+        start = start.toUpperCase(); end = end.toUpperCase();  //make sure these are Upper case
+        startField = start;      //set the global fields for printing when no ladder exists
+        endField = end;
+        deapthVisited.clear();      //clear the list of visited words
+        return getWLDFS(start, end);    //start recursion
     }
 	
     public static ArrayList<String> getWordLadderBFS(String start, String end) {  //iterative
-        return getWordLadderBFS(new Node(start.toUpperCase()),new Node(end.toUpperCase()));
+        breadthVisited.clear();     //clear the list of vistied words
+        startField = start;      //set the global fields for printing when no ladder exists
+        endField = end;
+        return getWordLadderBFS(new Node(start.toUpperCase()),new Node(end.toUpperCase())); //start iteration
     }
     
-	public static Set<String>  makeDictionary () {
+	public static Set<String>  makeDictionary () {  //provided function
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
 		try {
@@ -185,65 +138,94 @@ public class Main {
 		return words;
 	}
 	
-	public static void printLadder(ArrayList<String> ladder) {
-        if(ladder.isEmpty()){ //TODO how to get start and finish?
-            System.out.println("no word ladder can be found between <start> and <finish>");
+	public static void printLadder(ArrayList<String> ladder) {      //prints the word ladder
+        if(ladder.size()==0){       //only if no ladder exists
+            System.out.println("no word ladder can be found between "+startField.toLowerCase()+" and "+endField.toLowerCase());
             return;
         }
-        System.out.println("a "+(ladder.size()-2)+"-rung word ladder exists between "+ladder.get(ladder.size()-1).toLowerCase()+" and "+ladder.get(0).toLowerCase()+".");
+        System.out.println("a "+(ladder.size()-2)+"-rung word ladder exists between "+startField.toLowerCase()+" and "+endField.toLowerCase()+".");     //0+ ladders print
         for(int i = ladder.size()-1; i>=0; i--){
-            System.out.println(ladder.get(i).toLowerCase());
+            System.out.println(ladder.get(i).toLowerCase());    //print all the words of ladder
         }
 	}
 
     private static ArrayList<String> getWordLadderBFS(Node start, Node end){
-        if(start.equals(end)){
-            return new ArrayList<String>();
+        if(start.equals(end)){      //if start equals en (not tested)
+            return new ArrayList<String>(); // return empty array
         }
-        LinkedList<Node> Q = new LinkedList<Node>();                        //Queue implementation
-        Q.push(start);
-        breadthVisited.add(start.toString());
-        while(!Q.isEmpty()) {
-            Node root = Q.remove();
-            if(root.equals(end)){
-                ArrayList<String> value = new ArrayList<String>();
-                value.addAll(root.getFamilyTree(root));
-                breadthVisited.clear();
-                return value;
+        ArrayDeque<Node> Q = new ArrayDeque<Node>();                        //Queue implementation
+        Q.push(start);      //add first word to Q
+        breadthVisited.add(start.toString());   //add first word to visited
+        while(!Q.isEmpty()) {       //while there is a possible path
+            Node root = Q.remove();     //remove first element from Q
+            if(root.equals(end)){   //if that element is the end
+                ArrayList<String> value = new ArrayList<String>();  //create ArrayList
+                value.addAll(root.getFamilyTree(root)); //add end and all its parents to Array
+                breadthVisited.clear(); //clear the visited list
+                return value;       //return the Array
             }
-            ArrayList<String> nextWords = adjacents.get(root.toString());
-            if(nextWords.contains(end.toString())){
-                Q.push(end.setParent(root));
+            HashSet<String> nextWords = adjacents.get(root.toString()); //get list of adjacent words from hashmap
+            if(nextWords.contains(end.toString())){ //if that set of words contains the end word
+                Q.push(end.setParent(root));    //put the end word at the front of the Q (setting parent to this word)
             }
+            //for each word that has not been visited yet - mark that word visited and add it to Q (setting parent to this word)
             nextWords.stream().filter(word -> !breadthVisited.contains(word)).forEach(word -> {breadthVisited.add(word); Q.add(new Node(word).setParent(root));});
         }
-        return new ArrayList<String>();
+        return new ArrayList<String>(); //if no ladder is found return empty ArrayList
     }
 
-    private static ArrayList<String> getWLDFS(String start, String end) {
-        start = start.toUpperCase(); end = end.toUpperCase();
+    private static ArrayList<String> getWLDFS(String start, String end) {   //recursive
         ArrayList<String> value = new ArrayList<String>();      //value to return
-        if(start.equals(end)){
-            value.add(end);
+        if(start.equals(end)){          //if we found the end
+            value.add(end);         //add the end to the list
+            deapthVisited.remove(end);      //remove from visited list
+            return value;           //return the ArrayList
+        }
+        if(deapthVisited.contains(start)){      //if weve visited this word return
             return value;
         }
-        if(deapthVisited.contains(start)){
-            return value;
+        deapthVisited.add(start);       //mark current word visited
+        HashSet<String> nextWords = adjacents.get(start);       //get the adjacent words from hashmap
+        if(nextWords.contains(end)){        //if this word is adjacent to the end
+            value = getWLDFS(end,end);      //go to the end
+            value.add(start);               //add this word to list
+            deapthVisited.remove(start);        //remove this word from visited
+            return value;               //return the array
         }
-        deapthVisited.add(start);
-        ArrayList<String> nextWords = adjacents.get(start);
-        if(nextWords.contains(end)){
-            getWLDFS(end,end);
-        }
-        for(String word:nextWords){
-            if(!deapthVisited.contains(word)) {
-                value = getWLDFS(word, end);
-                if (!value.isEmpty()) {
-                    value.add(start);
-                    return value;
+        for(String word:nextWords){     //for every adjacent word
+            if(!deapthVisited.contains(word)) {     //if we have not visited that word
+                value = getWLDFS(word, end);        //call DFS on that word
+                if (!value.isEmpty()) {             //if we recieved a return that is not empty (found the end)
+                    value.add(start);               //add this word to array
+                    deapthVisited.remove(start);    //remove this word from visited
+                    return value;                   //return this array
                 }
             }
         }
-        return value;
+        return value;       //return empty array for no ladder
+    }
+
+    private void printAdjacents(){  //helper method for visiualizing graph
+        Iterator it = adjacents.entrySet().iterator();
+        try {
+            PrintStream ps = new PrintStream(new File("adjacents2.txt"));
+            System.setOut(ps);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.print(pair.getKey()+" : ");
+            ((HashSet<String>)pair.getValue()).stream().forEach(word -> System.out.print(word+", "));
+            System.out.println();
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+    }
+    @Test
+    public void testb1(){   //first test similar to example provided for verification of length
+        initialize();
+        ArrayList<String> temp = getWordLadderBFS("money","stone");
+        printLadder(temp);
     }
 }
